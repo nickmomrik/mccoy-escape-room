bd21eef4591faf373a7bb05d66cf47e1234da01a
Nick Momrik <nick@momrik.com>
2017-05-09 13:12:15 -0400

    Swap pot for a button. Turns out trying to read analog on digital sucks. Who would have thought?! Implement next phase output when code is correct. Update digits with button press. Need timing or event actions for button presses so it does not press many times for each press.

--- control-panel-1/main.py
+++ control-panel-1/main.py
@@ -3,18 +3,21 @@
 # Enable switch
 switch_pin = 4
 
-# Potentiameter in/out
-pot1_pin = 20
-pot2_pin = 21
+# Buttons
+button1_pin = 20
+button2_pin = 27
 
-# Button
-button_pin = 27
+# Next Phase
+next_phase_pin = 21
 
 # Shared 7 Segment Display Pins (A, B, C, D, E, F, G, DP)
 segment_pins = [ 18, 23, 24, 25, 8, 7, 12, 16 ]
 
 # Individual 7 Segment Displays
 digit_pins = [ 22, 5, 6, 13, 19, 26 ]
+
+# Correct code
+secret_code = [ 1, 0, 0, 0, 0, 0 ]
 
 ############
 # END CONFIG
@@ -23,10 +26,26 @@ digit_pins = [ 22, 5, 6, 13, 19, 26 ]
 import RPi.GPIO as GPIO
 import time
 
+# Segment on/off for each number
+numbers = {
+  0: ( 1, 1, 1, 1, 1, 1, 0 ),
+  1: ( 0, 1, 1, 0, 0, 0, 0 ),
+  2: ( 1, 1, 0, 1, 1, 0, 1 ),
+  3: ( 1, 1, 1, 1, 0, 0, 1 ),
+  4: ( 0, 1, 1, 0, 0, 1, 1 ),
+  5: ( 1, 0, 1, 1, 0, 1, 1 ),
+  6: ( 1, 0, 1, 1, 1, 1, 1 ),
+  7: ( 1, 1, 1, 0, 0, 0 ,0 ),
+  8: ( 1, 1, 1, 1, 1, 1, 1 ),
+  9: ( 1, 1, 1, 1, 0, 1, 1 )
+}
+
 # Setup the pins
 GPIO.setmode( GPIO.BCM )
-GPIO.setup( switch_pin,  GPIO.IN, pull_up_down = GPIO.PUD_DOWN )  #enable switch
-GPIO.setup( button_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP )  #button
+GPIO.setup( switch_pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN )
+GPIO.setup( button1_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP )
+GPIO.setup( button2_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP )
+GPIO.setup( next_phase_pin, GPIO.OUT, initial = GPIO.LOW )
 GPIO.setup( segment_pins, GPIO.OUT, initial = GPIO.LOW )
 GPIO.setup( digit_pins, GPIO.OUT, initial = GPIO.LOW )
 
@@ -36,28 +55,19 @@ digit_values = [ 0, 0, 0, 0, 0, 0 ]
 next_phase = False
 selected_digit = 0
 
-# Setup pot for capacitor data
-def discharge_pot() :
-  GPIO.setup( pot1_pin, GPIO.IN )
-  GPIO.setup( pot2_pin, GPIO.OUT, initial = GPIO.LOW )
-  time.sleep( 0.005 )
+def reset() :
+  enabled = False
+  next_phase = False
+  selected_digit = 0
+  for i in range( len( digit_values ) ) :
+    digit_values[i] = 0
 
-# Capture analog value
-def pot_charge_time() :
-  GPIO.setup( pot2_pin, GPIO.IN )
-  GPIO.setup( pot1_pin, GPIO.OUT, initial = GPIO.HIGH )
+def code_is_valid() :
+  for i in range( len( digit_values ) ) :
+    if ( not secret_code[i] == digit_values[i] ) :
+      return False
 
-  start = time.clock()
-  while ( not GPIO.input( pot2_pin ) ) :
-    pass
-
-  return int( round( ( time.clock() - start ) * 1000 ) )
-
-# Reading charging and discharging data
-def read_pot() :
-  discharge_pot()
-
-  return pot_charge_time()
+  return True
 
 time.clock()
 
@@ -69,26 +79,28 @@ try :
         print "Switch enabled"
 
     if ( GPIO.LOW == GPIO.input( switch_pin ) ) :
-      enabled = False
-      if ( next_phase ) :
-        next_phase = False
+      reset()
     else :
-      pot = read_pot()
-      #print "pot: " + str( pot )
-      #selected_digit = map
+      GPIO.output( next_phase_pin, next_phase )
 
-      if ( not GPIO.input( button_pin ) ) :
+      if ( not GPIO.input( button1_pin ) ) :
+        current_value = digit_values[selected_digit]
+        if ( 9 == current_value ) :
+          digit_values[selected_digit] = 0
+        else :
+          digit_values[selected_digit] += 1
+
+        next_phase = code_is_valid()
+
+      if ( not GPIO.input( button2_pin ) ) :
         if ( selected_digit < len( digit_values ) ) :
           selected_digit += 1
         else :
           selected_digit = 0
-        #while ( not GPIO.input( button_pin ) ) :
-        #  pass
-        print "button: " + str( selected_digit )
 
       for i in range( len( digit_values ) ) :
         for j in range( 7 ) :
-          GPIO.output( segment_pins[j], GPIO.HIGH )
+          GPIO.output( segment_pins[j], numbers[ digit_values[i] ][j] )
         if ( i == selected_digit ) :
           GPIO.output( segment_pins[7], GPIO.HIGH )
         else :
