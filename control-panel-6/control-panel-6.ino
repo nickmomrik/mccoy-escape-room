d3305145bdb74b37f5e8bc84f2632caecacbd979
Nick Momrik <nick@momrik.com>
2017-06-28 22:23:59 -0400

    control-panel-6: introduce the NeoPixel FeatherWing by strolling the solution text.

--- control-panel-6/control-panel-6.ino
+++ control-panel-6/control-panel-6.ino
@@ -1,3 +1,7 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+
 // Analog Pins
 #define PHOTOCELL_PIN 0
 #define POT_PIN 1
@@ -8,12 +12,29 @@
 #define MAGNET_SWITCH_PIN 10
 #define ENABLE_SWITCH_PIN 9
 
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix( 4, 8, 6,
+  NEO_MATRIX_TOP     + NEO_MATRIX_RIGHT +
+  NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE,
+  NEO_GRB            + NEO_KHZ800 );
+
+const uint16_t orange = matrix.Color( 255, 69, 0 );
+int photocell_read;
+long ultrasonic_read;
+bool magnet_read;
+int pot_read;
+bool enable_read;
+int x = matrix.width();
 
 void setup() {
   pinMode( ULTRA_TRIG_PIN, OUTPUT );
   pinMode( ULTRA_ECHO_PIN, INPUT );
   pinMode( MAGNET_SWITCH_PIN, INPUT );
   pinMode( ENABLE_SWITCH_PIN, INPUT );
+
+  matrix.begin();
+  matrix.setTextWrap( false );
+  matrix.setBrightness( 40 );
+  matrix.setTextColor( orange );
 
   Serial.begin( 9600 );
 }
@@ -32,12 +53,6 @@ long read_ultrasonic() {
 
   return ( ultra_duration / 2 ) / 29.1;
 }
-
-int photocell_read;
-long ultrasonic_read;
-bool magnet_read;
-int pot_read;
-bool enable_read;
 
 void loop() {
   enable_read = digitalRead( ENABLE_SWITCH_PIN );
@@ -60,8 +75,16 @@ void loop() {
     Serial.println( pot_read );
   
     Serial.println( "" );
+
+    matrix.fillScreen( 0 );
+    matrix.setCursor( x, 0 );
+    matrix.print( F( "ORANGE=1  " ) );
+    if ( --x < -48 ) {
+      x = matrix.width();  
+    }
+    matrix.show();
   }
 
-  delay( 500 );
+  delay( 100 );
 }
 
