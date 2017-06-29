842156561ca2538b7a993db6d5399064da882851
Nick Momrik <nick@momrik.com>
2017-06-28 21:33:53 -0400

    control-panel-6: add magnet switch read

--- control-panel-6/control-panel-6.ino
+++ control-panel-6/control-panel-6.ino
@@ -4,6 +4,7 @@
 // Digital Pins
 #define ULTRA_TRIG_PIN 12
 #define ULTRA_ECHO_PIN 11
+#define MAGNET_SWITCH_PIN 10
 
 
 void setup() {
@@ -11,26 +12,14 @@ void setup() {
   digitalWrite( ULTRA_TRIG_PIN, LOW );
   pinMode( ULTRA_ECHO_PIN, INPUT );
 
+  pinMode( MAGNET_SWITCH_PIN, INPUT );
+
   Serial.begin( 9600 );
 }
-
-int photocell_read;
-long ultrasonic_read;
 
 long read_ultrasonic() {
   long ultra_duration;
   long ultra_distance;
-
-  ultra_duration = pulseIn( ULTRA_ECHO_PIN, HIGH );
-
-  return ( ultra_duration / 2 ) / 29.1;
-}
-
-void loop() {
-  photocell_read = analogRead( PHOTOCELL_PIN );
-
-  Serial.print( "Photo: " );
-  Serial.println( photocell_read );
 
   digitalWrite( ULTRA_TRIG_PIN, LOW );
   delay( 50 );
@@ -38,10 +27,30 @@ void loop() {
   delay( 20 );
   digitalWrite( ULTRA_TRIG_PIN, LOW );
 
+  ultra_duration = pulseIn( ULTRA_ECHO_PIN, HIGH );
+
+  return ( ultra_duration / 2 ) / 29.1;
+}
+
+int photocell_read;
+long ultrasonic_read;
+bool magnet_read;
+
+void loop() {
+  photocell_read = analogRead( PHOTOCELL_PIN );
+  Serial.print( "Photo: " );
+  Serial.println( photocell_read );
+
   ultrasonic_read = read_ultrasonic();
   Serial.print( "Ultrasonic: " );
   Serial.print( ultrasonic_read );
-  Serial.println(" cm");
+  Serial.println( " cm" );
+
+  magnet_read = digitalRead( MAGNET_SWITCH_PIN );
+  Serial.print( "Magnet: " );
+  Serial.println( magnet_read );
+
+  Serial.println( "" );
 
   delay( 500 );
 }
